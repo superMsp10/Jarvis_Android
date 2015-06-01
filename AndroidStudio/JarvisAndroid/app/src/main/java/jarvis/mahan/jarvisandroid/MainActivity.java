8759f69c38cb35e72f962f72e687ade34dcd4f9d
superMsp10 <pandeymahan34@gmail.com>
2015-05-31 20:20:07 -0400

    aCTION sEARCH
    implemented action search button to respond with hi in the console

--- AndroidStudio/JarvisAndroid/app/src/main/java/jarvis/mahan/jarvisandroid/MainActivity.java
+++ AndroidStudio/JarvisAndroid/app/src/main/java/jarvis/mahan/jarvisandroid/MainActivity.java
@@ -2,6 +2,7 @@ package jarvis.mahan.jarvisandroid;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -19,7 +20,7 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -31,9 +32,22 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            openSettings();
+            return true;
+        }else if(id == R.id.action_search){
+
+            openSearch();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void openSettings() {
+
+    }
+
+    private void openSearch() {
+        Log.i("hi", "HI");
     }
 }
