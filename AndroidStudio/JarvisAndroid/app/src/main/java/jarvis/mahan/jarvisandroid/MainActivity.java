cd53b29e420ab434732063c48634b9154a19338d
superMsp10 <pandeymahan34@gmail.com>
2015-07-23 09:34:30 -0400

    icon change

--- AndroidStudio/JarvisAndroid/app/src/main/java/jarvis/mahan/jarvisandroid/MainActivity.java
+++ AndroidStudio/JarvisAndroid/app/src/main/java/jarvis/mahan/jarvisandroid/MainActivity.java
@@ -1,34 +1,24 @@
 package jarvis.mahan.jarvisandroid;
 
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    private ImageView logoPicture;
-    private Drawable lady_logoPicture;
-    private Drawable bullDog_logoPicture;
 
-    private boolean changedLogo = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-       lady_logoPicture = getResources().getDrawable(R.drawable.capture);
-       bullDog_logoPicture = getResources().getDrawable(R.drawable.bulldogimage);
-       logoPicture = (ImageView)findViewById(R.id.imageView);
 
         final Button hubButton = (Button) findViewById(R.id.tohubButton);
         hubButton.setOnClickListener(new View.OnClickListener() {
@@ -77,10 +67,12 @@ public class MainActivity extends ActionBarActivity {
         startActivity(intent);
 
     }
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+       getMenuInflater().inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -95,8 +87,6 @@ public class MainActivity extends ActionBarActivity {
         if (id == R.id.action_settings) {
             return true;
         } else if (id == R.id.action_search) {
-
-            openSearch();
             return true;
         }
 
@@ -104,16 +94,7 @@ public class MainActivity extends ActionBarActivity {
     }
 
 
-    private void openSearch() {
-        if (!changedLogo) {
-            logoPicture.setImageDrawable(bullDog_logoPicture);
-            changedLogo = true;
-        } else {
-            logoPicture.setImageDrawable(lady_logoPicture);
 
-            changedLogo = false;
-        }
 
-    }
 
 }
