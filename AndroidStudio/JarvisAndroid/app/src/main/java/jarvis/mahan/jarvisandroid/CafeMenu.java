e00f01a5a461685ce459bb9bfe72b21d199b93c4
superMsp10 <pandeymahan34@gmail.com>
2015-08-07 20:54:37 -0400

    create cells according to data

--- AndroidStudio/JarvisAndroid/app/src/main/java/jarvis/mahan/jarvisandroid/CafeMenu.java
+++ AndroidStudio/JarvisAndroid/app/src/main/java/jarvis/mahan/jarvisandroid/CafeMenu.java
@@ -4,11 +4,30 @@ import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
@@ -33,15 +52,131 @@ public class CafeMenu extends AppCompatActivity {
         );
         setTitle("Cafeteria Menu");
 
+
+        getData();
+
+
+    }
+
+
+    void getData() {
+
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://jarvisapp-supermsp10.rhcloud.com/CafeMenu";
+
+
+        // Request a string response from the provided URL.
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    JSONArray res;
+
+                    @Override
+                    public void onResponse(String response) {
+
+
+                        try {
+                            res = new JSONArray(response);
+                            useData(res);
+                            // Log.println(Log.ASSERT, "Response from server", res.toString());
+                            writeToFile(res.toString());
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Log.println(Log.ASSERT, "Failed database json", "failed");
+
+                        }
+
+                    }
+
+                    private void writeToFile(String data) {
+                        try {
+                            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(openFileOutput("CafeMenu.txt", Context.MODE_PRIVATE));
+                            outputStreamWriter.write(data);
+                            outputStreamWriter.close();
+                        } catch (IOException e) {
+                            Log.e("Exception", "File write failed: " + e.toString());
+                        }
+                    }
+
+
+                }, new Response.ErrorListener() {
+            JSONArray res;
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                try {
+                    res = new JSONArray(readFromFile());
+                    useData(res);
+                    //Log.println(Log.ASSERT, "Data from file", res.toString());
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Log.println(Log.ASSERT, "Failed json from file", "failed");
+
+                }
+            }
+
+            private String readFromFile() {
+
+                String ret = "";
+
+                try {
+                    InputStream inputStream = openFileInput("CafeMenu.txt");
+
+                    if (inputStream != null) {
+                        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                        String receiveString;
+                        StringBuilder stringBuilder = new StringBuilder();
+
+                        while ((receiveString = bufferedReader.readLine()) != null) {
+                            stringBuilder.append(receiveString);
+                        }
+
+                        inputStream.close();
+                        ret = stringBuilder.toString();
+                    }
+                } catch (FileNotFoundException e) {
+                    Log.e("login activity", "File not found: " + e.toString());
+                } catch (IOException e) {
+                    Log.e("login activity", "Can not read file: " + e.toString());
+                }
+
+                return ret;
+            }
+        }
+
+
+        );
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
+
+    }
+
+    void useData(JSONArray data) throws JSONException {
         int displayHeight = getWindowManager().getDefaultDisplay().getHeight();
+        int cellHeight = displayHeight / 6;
         LinearLayout s = (LinearLayout) findViewById(R.id.CafeLinearlayout);
 
         LayoutInflater inflater = LayoutInflater.from(this);
-        inflater.inflate(R.layout.cafemenucell, s);
+        for (int i = 0; i < data.length(); i++) {
+            String name = data.getJSONObject(i).getString("Date");
+            inflater.inflate(R.layout.cafemenucell, s);
 
-        TextView t = (TextView) s.getChildAt(0);
-        t.setText("hello");
+            TextView t = (TextView) s.getChildAt(i);
+            t.getLayoutParams().height = cellHeight;
+            t.requestLayout();
 
+            t.setText(name);
+
+        }
+        JSONObject j = data.getJSONObject(0);
+        Log.println(Log.ASSERT, "Data", j.toString());
 
 
 
--- AndroidStudio/JarvisAndroid/app/src/main/java/jarvis/mahan/jarvisandroid/MainActivity.java
+++ AndroidStudio/JarvisAndroid/app/src/main/java/jarvis/mahan/jarvisandroid/MainActivity.java
@@ -7,28 +7,10 @@ import android.graphics.PorterDuff;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
@@ -60,128 +42,31 @@ public class MainActivity extends AppCompatActivity {
         setTitle("Jarvis");
 
         int displayHeight = getWindowManager().getDefaultDisplay().getHeight();
-        Button cafe = (Button)findViewById(R.id.CafeMenuButton);
+        Button cafe = (Button) findViewById(R.id.CafeMenuButton);
         cafe.getLayoutParams().height = displayHeight / 6;
 
-        Button hub = (Button)findViewById(R.id.tohubButton);
+        Button hub = (Button) findViewById(R.id.tohubButton);
         hub.getLayoutParams().height = displayHeight / 6;
 
-        Button news = (Button)findViewById(R.id.NewsFeed);
+        Button news = (Button) findViewById(R.id.NewsFeed);
         news.getLayoutParams().height = displayHeight / 6;
 
-        Button calendar = (Button)findViewById(R.id.calenderButton);
+        Button calendar = (Button) findViewById(R.id.calenderButton);
         calendar.getLayoutParams().height = displayHeight / 6;
 
-        TextView text = (TextView)findViewById(R.id.welcomLabel);
-        text.getLayoutParams().height = (displayHeight / 6)/3;
-        text = (TextView)findViewById(R.id.dateLabel);
-        text.getLayoutParams().height = (displayHeight / 6)/3;
-        text = (TextView)findViewById(R.id.dayAndPeriodLabel);
-        text.getLayoutParams().height = (displayHeight / 6)/3;
-
-
-
-    // Instantiate the RequestQueue.
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="http://jarvisapp-supermsp10.rhcloud.com/CafeMenu";
-
-
-    // Request a string response from the provided URL.
-
-        StringRequest  stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    JSONArray res;
-
-                    @Override
-                    public void onResponse(String response) {
-
-
-                        try {
-                             res = new JSONArray(response);
-
-                            // Log.println(Log.ASSERT, "Response from server", res.toString());
-                            writeToFile(res.toString());
-
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                            Log.println(Log.ASSERT, "Failed database json", "failed");
-
-                        }
-
-                    }
-
-                    private void writeToFile(String data) {
-                        try {
-                            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(openFileOutput("CafeMenu.txt", Context.MODE_PRIVATE));
-                            outputStreamWriter.write(data);
-                            outputStreamWriter.close();
-                        } catch (IOException e) {
-                            Log.e("Exception", "File write failed: " + e.toString());
-                        }
-                    }
-
-
-
-                }, new Response.ErrorListener() {
-            JSONArray res;
-
-            @Override
-            public void onErrorResponse(VolleyError error) {
-
-                try {
-                    res = new JSONArray(readFromFile());
-
-                    //Log.println(Log.ASSERT, "Data from file", res.toString());
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                    Log.println(Log.ASSERT, "Failed json from file", "failed");
-
-                }
-            }
-
-            private String readFromFile() {
-
-                String ret = "";
-
-                try {
-                    InputStream inputStream = openFileInput("CafeMenu.txt");
-
-                    if (inputStream != null) {
-                        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-                        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-                        String receiveString;
-                        StringBuilder stringBuilder = new StringBuilder();
-
-                        while ((receiveString = bufferedReader.readLine()) != null) {
-                            stringBuilder.append(receiveString);
-                        }
-
-                        inputStream.close();
-                        ret = stringBuilder.toString();
-                    }
-                } catch (FileNotFoundException e) {
-                    Log.e("login activity", "File not found: " + e.toString());
-                } catch (IOException e) {
-                    Log.e("login activity", "Can not read file: " + e.toString());
-                }
-
-                return ret;
-            }
-        }
-
-
-        );
-    // Add the request to the RequestQueue.
-        queue.add(stringRequest);
-
+        TextView text = (TextView) findViewById(R.id.welcomLabel);
+        text.getLayoutParams().height = (displayHeight / 6) / 3;
+        text = (TextView) findViewById(R.id.dateLabel);
+        text.getLayoutParams().height = (displayHeight / 6) / 3;
+        text = (TextView) findViewById(R.id.dayAndPeriodLabel);
+        text.getLayoutParams().height = (displayHeight / 6) / 3;
 
 
         final Button hubButton = (Button) findViewById(R.id.tohubButton);
         buttonEffect(hubButton);
         hubButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-               changeToHub();
+                changeToHub();
             }
         });
 
@@ -210,19 +95,18 @@ public class MainActivity extends AppCompatActivity {
         buttonEffect(newsButton);
 
 
-
     }
 
     private void changeToHub() {
 
         Intent intent;
-        intent =  new Intent(this,Hub.class);
+        intent = new Intent(this, Hub.class);
         startActivity(intent);
 
     }
 
 
-    public static void buttonEffect(View button){
+    public static void buttonEffect(View button) {
         button.setOnTouchListener(new View.OnTouchListener() {
 
             public boolean onTouch(View v, MotionEvent event) {
@@ -246,7 +130,7 @@ public class MainActivity extends AppCompatActivity {
     private void changeToCafeMenu() {
 
         Intent intent;
-        intent =  new Intent(this,CafeMenu.class);
+        intent = new Intent(this, CafeMenu.class);
         startActivity(intent);
 
     }
@@ -254,14 +138,10 @@ public class MainActivity extends AppCompatActivity {
     private void changeToCalender() {
 
         Intent intent;
-        intent =  new Intent(this,Calender.class);
+        intent = new Intent(this, Calender.class);
         startActivity(intent);
 
     }
-
-
-
-
 
 
 }
