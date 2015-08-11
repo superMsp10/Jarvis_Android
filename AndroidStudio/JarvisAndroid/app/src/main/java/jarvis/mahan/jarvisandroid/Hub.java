a43d2f564b5177589d50b608a598bdfdda5bdebb
superMsp10 <pandeymahan34@gmail.com>
2015-08-11 15:58:06 -0400

    hub cells

--- AndroidStudio/JarvisAndroid/app/src/main/java/jarvis/mahan/jarvisandroid/Hub.java
+++ AndroidStudio/JarvisAndroid/app/src/main/java/jarvis/mahan/jarvisandroid/Hub.java
@@ -4,13 +4,17 @@ import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 
 public class Hub extends AppCompatActivity {
-
 
 
     @Override
@@ -30,16 +34,45 @@ public class Hub extends AppCompatActivity {
                         .setFontAttrId(R.attr.fontPath)
                         .build()
         );
-        int screenHeight = getWindowManager().getDefaultDisplay().getHeight();
-        int screenWidth = getWindowManager().getDefaultDisplay().getHeight();
-
-        int labelHeight = screenHeight/7;
-        int labelPos = (int)(labelHeight/1.5);
-        int textWidth = screenWidth -labelHeight;
-
         setTitle("Social HUB");
 
 
+        int screenHeight = getWindowManager().getDefaultDisplay().getHeight();
+        int screenWidth = getWindowManager().getDefaultDisplay().getHeight();
+
+        int labelHeight = screenHeight / 7;
+        int labelPos = (int) (labelHeight / 1.5);
+        int textWidth = screenWidth - labelHeight;
+
+
+        LinearLayout s = (LinearLayout) findViewById(R.id.hubMain);
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View space = new View(this);
+        View v;
+        space.setLayoutParams(new LinearLayout.LayoutParams(screenWidth, labelPos));
+
+
+        //Instagram
+        inflater.inflate(R.layout.hub_cell, s);
+        v = findViewById(R.id.hubCellButton);
+        v.getLayoutParams().height = labelHeight;
+        v.getLayoutParams().width = labelHeight;
+        v = findViewById(R.id.hubCellTextView);
+        v.getLayoutParams().height = labelHeight;
+        v.invalidate();
+        v.setId(0);
+
+
+
+        s.addView(space, 1);
+        inflater.inflate(R.layout.hub_cell, s);
+        v = findViewById(R.id.hubCellButton);
+        v.getLayoutParams().height = labelHeight;
+        v.getLayoutParams().width = labelHeight;
+        v = findViewById(R.id.hubCellTextView);
+        v.getLayoutParams().height = labelHeight;
+        v.invalidate();
+        v.setId(0);
 
 
     }
