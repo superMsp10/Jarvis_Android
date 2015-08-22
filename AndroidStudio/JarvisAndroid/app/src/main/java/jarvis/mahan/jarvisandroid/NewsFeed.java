0bc5cf6fa1c78ad9838ebbe74b7a16cc282302f4
superMsp10 <pandeymahan34@gmail.com>
2015-08-22 15:25:24 -0400

    get datan from server

--- AndroidStudio/JarvisAndroid/app/src/main/java/jarvis/mahan/jarvisandroid/NewsFeed.java
+++ AndroidStudio/JarvisAndroid/app/src/main/java/jarvis/mahan/jarvisandroid/NewsFeed.java
@@ -4,12 +4,33 @@ import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Comparator;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 
 public class NewsFeed extends AppCompatActivity {
+    private JSONArray menus;
 
     @Override
     protected void attachBaseContext(Context newBase) {
@@ -29,6 +50,140 @@ public class NewsFeed extends AppCompatActivity {
                         .build()
         );
         setTitle("News Feed");
+
+        getData();
+    }
+
+    JSONArray sortJsonArray(JSONArray start) throws JSONException {
+        Integer[] arr;
+        arr = new Integer[start.length()];
+        for (int i = 0; i < start.length(); i++) {
+            arr[i] = (start.getJSONObject(i).getInt("Epoch"));
+        }
+
+        Arrays.sort(arr, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer x, Integer y) {
+                return y - x;
+            }
+        });
+
+
+        JSONArray rs = new JSONArray();
+
+        for (int i = 0; i < start.length(); i++) {
+            //For each of the json objects in the res array
+
+            int index = Arrays.asList(arr).indexOf(start.getJSONObject(i).getInt("Epoch"));
+            //see where its epoch is in the integer array
+
+            rs.put(index, start.getJSONObject(i));
+            //assign the whole json object to that index
+        }
+
+        return rs;
+
+    }
+
+
+    void getData() {
+
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://jarvisapp-supermsp10.rhcloud.com/NewsFeed";
+
+
+        // Request a string response from the provided URL.
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    JSONArray res;
+
+                    @Override
+                    public void onResponse(String response) {
+
+
+                        try {
+                            res = sortJsonArray(new JSONArray(response));
+
+                            //  useData(res);
+                            // Log.println(Log.ASSERT, "Response from server", res.toString());
+                            writeToFile(res.toString());
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Log.println(Log.ASSERT, "Failed database json", "failed");
+
+                        }
+
+                    }
+
+                    private void writeToFile(String data) {
+                        try {
+                            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(openFileOutput("NewsFeed.txt", Context.MODE_PRIVATE));
+                            outputStreamWriter.write(data);
+                            outputStreamWriter.close();
+                        } catch (IOException e) {
+                            Log.e("Exception", "File write failed: " + e.toString());
+                        }
+                    }
+
+
+                }, new Response.ErrorListener() {
+            JSONArray res;
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                try {
+                    res = new JSONArray(readFromFile());
+                    // useData(res);
+                    //Log.println(Log.ASSERT, "Data from file", res.toString());
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Log.println(Log.ASSERT, "Failed json from file", "failed");
+
+                }
+            }
+
+            private String readFromFile() {
+
+                String ret = "";
+
+                try {
+                    InputStream inputStream = openFileInput("NewsFeed.txt");
+
+                    if (inputStream != null) {
+                        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                        String receiveString;
+                        StringBuilder stringBuilder = new StringBuilder();
+
+                        while ((receiveString = bufferedReader.readLine()) != null) {
+                            stringBuilder.append(receiveString);
+                        }
+
+                        inputStream.close();
+                        ret = stringBuilder.toString();
+                    }
+                } catch (FileNotFoundException e) {
+                    Log.e("login activity", "File not found: " + e.toString());
+                } catch (IOException e) {
+                    Log.e("login activity", "Can not read file: " + e.toString());
+                }
+
+                return ret;
+            }
+        }
+
+
+        );
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
+
     }
 
 
